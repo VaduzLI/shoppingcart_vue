cadd20244170b9fe7606fe3f17cee57294faf09b
VaduzLI <56792993+VaduzLI@users.noreply.github.com>
2020-06-15 21:59:12 +0200

    Category from static dynamic from API

--- shoppingcart/src/views/Category.vue
+++ shoppingcart/src/views/Category.vue
@@ -1,8 +1,12 @@
 <template>
   <v-content>
     <v-container>
-      <v-row class="mt-12">
-        <v-col cols="12">
+      <v-row v-if="!isFetching" class="mt-12">
+        <v-col
+          v-for="category in categories"
+          :key="category.category_id"
+          cols="12"
+        >
           <v-card justify="center">
             <v-row align="center" justify="center" min-height="260">
               <v-col align="center" cols="6">
@@ -13,133 +17,20 @@
               <v-col cols="6">
                 <v-row align="center" justify="center">
                   <v-col cols="12">
-                    <h1 class="text-center display-1">Arrow Deploy</h1>
+                    <h1 class="text-center display-1">
+                      {{ category.category_name }}
+                    </h1>
                   </v-col>
                   <v-col cols="8">
                     <p class="subtitle-2">
-                      With the power of Arrow deploy you are capeble of
-                      launching many website, web application and so much more
+                      {{ category.category_description }}
                     </p>
                   </v-col>
                   <v-col class="text-center" cols="12">
-                    <v-btn href="/shop/arrow" align="center" color="primary"
-                      >Select Options</v-btn
-                    >
-                  </v-col>
-                </v-row>
-              </v-col>
-            </v-row>
-          </v-card>
-        </v-col>
-        <v-col cols="12">
-          <v-card justify="center">
-            <v-row align="center" justify="center" min-height="260">
-              <v-col cols="6">
-                <v-row align="center" justify="center">
-                  <v-col cols="12">
-                    <h1 class="text-center display-1">Website</h1>
-                  </v-col>
-                  <v-col cols="8">
-                    <p class="subtitle-2">
-                      Launch a website no strings attached. Upload your files
-                      and have it life and running in 5 minuts
-                    </p>
-                  </v-col>
-                  <v-col class="text-center" cols="12">
-                    <v-btn href="/shop/website" align="center" color="primary"
-                      >Select Options</v-btn
-                    >
-                  </v-col>
-                </v-row>
-              </v-col>
-              <v-col align="center" cols="6">
-                <div
-                  style="height: 200px; width: 350px; background-color: gray"
-                ></div>
-              </v-col>
-            </v-row>
-          </v-card>
-        </v-col>
-        <v-col cols="12">
-          <v-card justify="center">
-            <v-row align="center" justify="center" min-height="260">
-              <v-col align="center" cols="6">
-                <div
-                  style="height: 200px; width: 350px; background-color: gray"
-                ></div>
-              </v-col>
-              <v-col cols="6">
-                <v-row align="center" justify="center">
-                  <v-col cols="12">
-                    <h1 class="text-center display-1">Database</h1>
-                  </v-col>
-                  <v-col cols="8">
-                    <p class="subtitle-2">
-                      A Database always reachble. Choose the database that suits
-                      you the best
-                    </p>
-                  </v-col>
-                  <v-col class="text-center" cols="12">
-                    <v-btn href="/shop/database" align="center" color="primary"
-                      >Select Options</v-btn
-                    >
-                  </v-col>
-                </v-row>
-              </v-col>
-            </v-row>
-          </v-card>
-        </v-col>
-        <v-col cols="12">
-          <v-card justify="center">
-            <v-row align="center" justify="center" min-height="260">
-              <v-col cols="6">
-                <v-row align="center" justify="center">
-                  <v-col cols="12">
-                    <h1 class="text-center display-1">Container</h1>
-                  </v-col>
-                  <v-col cols="8">
-                    <p class="subtitle-2">
-                      Use the container to deploy more advanced projects and
-                      combine contaier for a ultimate solution.
-                    </p>
-                  </v-col>
-                  <v-col class="text-center" cols="12">
-                    <v-btn href="/shop/container" align="center" color="primary"
-                      >Select Options</v-btn
-                    >
-                  </v-col>
-                </v-row>
-              </v-col>
-              <v-col align="center" cols="6">
-                <div
-                  style="height: 200px; width: 350px; background-color: gray"
-                ></div>
-              </v-col>
-            </v-row>
-          </v-card>
-        </v-col>
-        <v-col cols="12">
-          <v-card justify="center">
-            <v-row align="center" justify="center" min-height="260">
-              <v-col align="center" cols="6">
-                <div
-                  style="height: 200px; width: 350px; background-color: gray"
-                ></div>
-              </v-col>
-              <v-col cols="6">
-                <v-row align="center" justify="center">
-                  <v-col cols="12">
-                    <h1 class="text-center display-1">Kluster</h1>
-                  </v-col>
-                  <v-col cols="8">
-                    <p class="subtitle-2">
-                      Manage everything with your cluster. Deploy simple
-                      webservices or ground breaking solutions. everything is
-                      possible with the kluster
-                    </p>
-                  </v-col>
-                  <v-col class="text-center" cols="12">
-                    <v-btn href="/shop/kluster" align="center" color="primary"
+                    <v-btn
+                      :href="'/shop/category/' + category.category_id"
+                      align="center"
+                      color="primary"
                       >Select Options</v-btn
                     >
                   </v-col>
@@ -152,3 +43,23 @@
     </v-container>
   </v-content>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      isFetching: true,
+      categories: null
+    };
+  },
+
+  mounted: function() {
+    axios.get("http://localhost:5000/api/category").then(res => {
+      this.categories = res.data;
+      this.isFetching = false;
+    });
+  }
+};
+</script>
